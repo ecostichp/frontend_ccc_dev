6da3a380c054ad39583e7f109c2bffb06af2bad0
onnymm <phr2807@gmail.com>
2024-05-28 13:37:54 -0700

    Arreglado problema de alineación y redimensionamiento de componentes en el apartado de inicio

--- src/components/cards/info-card.jsx
+++ src/components/cards/info-card.jsx
@@ -2,14 +2,14 @@ import LinkButton from "../../ui-kit/buttons/link-button";
 import ImageParallaxTemplate from "../../ui-kit/image-parallax-template/image-parallax-template";
 import style from './info-card.module.css';
 
-const InfoCard = ({src, title, description, button, className, link}) => {
+const InfoCard = ({ src, title, description, button, className, link }) => {
 
     return (
         <div className={`${style.container} ${className}`}>
             <ImageParallaxTemplate src={src} className={style.infoCard} />
             <div className={style.description}>
                 <div className={style.info}>
-                    <h2 className="fc-red fw-400">{title}</h2>
+                    <h2 className={`${style.title} fc-red fw-400`}>{title}</h2>
                     <p className="fc-gray">
                         {description}
                     </p>
