7f1cfedba4e652066f0c757377ceeeb2f0e8317c
onnymm <phr2807@gmail.com>
2024-06-05 10:44:37 -0700

    Resuelto el problema de logo no encontrado en la sección de sucursal al recargar la página

--- src/components/location-card.jsx
+++ src/components/location-card.jsx
@@ -5,12 +5,12 @@ import style from './location-card.module.css';
 const LocationCard = ({src, place, link}) => {
 
     return (
-        <NavLink to={`/sucursal/${link}`}>
+        <NavLink to={`/${link}`}>
             <div className={style.container}>
                 <img src={src}/>
                 <div className={style.content}>
                     <p>{place}</p>
-                    <LinkButton text={"Explorar"} link={`/sucursal/${link}`} />
+                    <LinkButton text={"Explorar"} link={`/${link}`} />
                 </div>
             </div>
         </NavLink>
